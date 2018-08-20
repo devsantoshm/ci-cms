fdc0b40d67b13c734b4673bef8dea296d62bd1a3
SantosHM <dosantoshm@gmail.com>
2018-08-20 16:57:24 -0500

    solve arguments for route

--- application/controllers/article.php
+++ application/controllers/article.php
@@ -9,26 +9,30 @@ class Article extends Frontend_Controller {
 		$this->data['recent_news'] = $this->article_m->get_recent();
 	}
 
-	public function index($id, $slug)
+	public function index($id = NULL, $slug = NULL)
 	{
-		// Fetch the article
-		$this->article_m->set_published();
-		$this->data['article'] = $this->article_m->get($id);
+		if (!is_null($id) && !is_null($slug)) {
+			// Fetch the article
+			$this->article_m->set_published();
+			$this->data['article'] = $this->article_m->get($id);
 
-		// Return 404 if not found uri_string() return blog/comments/123
-		count($this->data['article']) || show_404(uri_string());
+			// Return 404 if not found uri_string() return blog/comments/123
+			count($this->data['article']) || show_404(uri_string());
 
-		// Redirect if slug was incorrect
-		$request_slug = $this->uri->segment(3);
-		$set_slug = $this->data['article']->slug;
-		if ($request_slug != $set_slug) {
-			// with 301 redirect
-			redirect('article/' . $this->data['article']->id . '/' . $this->data['article']->slug, 'location', 301);
+			// Redirect if slug was incorrect
+			$request_slug = $this->uri->segment(3);
+			$set_slug = $this->data['article']->slug;
+			if ($request_slug != $set_slug) {
+				// with 301 redirect
+				redirect('article/' . $this->data['article']->id . '/' . $this->data['article']->slug, 'location', 301);
+			}
+			// Load view
+			add_meta_title($this->data['article']->title);
+			$this->data['subview'] = 'article';
+			$this->load->view('_main_layout', $this->data);
+		}else{
+			show_404(uri_string());
 		}
-		// Load view
-		add_meta_title($this->data['article']->title);
-		$this->data['subview'] = 'article';
-		$this->load->view('_main_layout', $this->data);
 	}
 }
 
